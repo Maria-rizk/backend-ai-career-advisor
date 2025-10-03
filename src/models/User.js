54fefd4619e63df15babf4a8d10ff9d27246cfb8
JoSsakr <sakrjo1@gmail.com>
2025-10-04 00:13:02 +0300

    git add A:\backend final\backend-ai-career-advisor\src\Controllers

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,47 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const UserSchema = new Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  name: { type: String, required: true },
+const { use } = require('react');
+const bcrypt = require('bcryptjs');
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  confirmPassword: {
+    type: String,
+    required: true
+  },
+  mobileNumber: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+UserSchema.pre("save" async function (next) {
+this.password = await bcrypt.hash (this.password, 12)
+next();
+}),
+UserSchema.pre("save" async function (next) {
+this.confirmPassword = await bcrypt.hash (this.confirmPassword, 12)
+next();
+}),
 });
-
 module.exports = mongoose.model('User', UserSchema);
